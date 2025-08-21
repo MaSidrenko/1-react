c15a99ab90bc513a9efda41e71cb0678f6da9863
MaSidrenko <nexon863@gmail.com>
2025-08-21 19:53:37 +0300

    MoveFloaders

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+
+import Header from '../header/Header.js';
+import Article from '../article/Article.js';
+import Footer from '../footer/Footer.js';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Article />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
